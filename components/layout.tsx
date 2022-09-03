1fa4b4d5af25077b31e78f33300ffa25ff409673
kth919 <kth9193@gmail.com>
2022-09-03 16:01:34 +0900

    :construction: 탭바 추가 -1

---
+++ components/layout.tsx
@@ -0,0 +1,136 @@
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import React from 'react';
+import { cls } from '../libs/utils';
+
+interface LayoutProps {
+  title?: string;
+  hasTabBar?: boolean;
+  canGoBack?: boolean;
+  children: React.ReactNode;
+}
+
+function Layout({ title, hasTabBar, canGoBack, children }: LayoutProps) {
+  const router = useRouter();
+
+  const handleGoBack = () => {
+    router.back();
+  };
+
+  return (
+    <div>
+      <div
+        className={cls(
+          'fixed top-0 z-50 flex w-full max-w-xl  items-center justify-center border-b bg-white py-3 px-10 text-lg font-medium text-gray-700',
+          !canGoBack ? 'justify-center' : '',
+        )}
+      >
+        {canGoBack && <button onClick={handleGoBack}>&larr;</button>}
+        {title && <span>{title}</span>}
+      </div>
+      <div className={cls(`pt-10`, hasTabBar ? 'pb-24' : '')}>{children}</div>
+
+      {hasTabBar && (
+        <nav className="fixed bottom-0 flex w-full max-w-xl justify-between border-t bg-white px-10 pb-5 pt-3 text-xs text-gray-700">
+          <Link href="/">
+            <div className="flex flex-col items-center space-y-2">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="h-6 w-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
+                />
+              </svg>
+              <span>홈</span>
+            </div>
+          </Link>
+          <Link href="/community">
+            <div className="flex flex-col items-center space-y-2">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                className="h-6 w-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M12 7.5h1.5m-1.5 3h1.5m-7.5 3h7.5m-7.5 3h7.5m3-9h3.375c.621 0 1.125.504 1.125 1.125V18a2.25 2.25 0 01-2.25 2.25M16.5 7.5V18a2.25 2.25 0 002.25 2.25M16.5 7.5V4.875c0-.621-.504-1.125-1.125-1.125H4.125C3.504 3.75 3 4.254 3 4.875V18a2.25 2.25 0 002.25 2.25h13.5M6 7.5h3v3H6v-3z"
+                />
+              </svg>
+
+              <span>동네생활</span>
+            </div>
+          </Link>
+          <Link href={'/chats'}>
+            <div className="flex flex-col items-center space-y-2">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="h-6 w-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M8.625 9.75a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H8.25m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H12m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0h-.375m-13.5 3.01c0 1.6 1.123 2.994 2.707 3.227 1.087.16 2.185.283 3.293.369V21l4.184-4.183a1.14 1.14 0 01.778-.332 48.294 48.294 0 005.83-.498c1.585-.233 2.708-1.626 2.708-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0012 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018z"
+                />
+              </svg>
+              <span>채팅</span>
+            </div>
+          </Link>
+          <Link href={'live'}>
+            <div className="flex flex-col items-center space-y-2">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="h-6 w-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  d="M15.75 10.5l4.72-4.72a.75.75 0 011.28.53v11.38a.75.75 0 01-1.28.53l-4.72-4.72M4.5 18.75h9a2.25 2.25 0 002.25-2.25v-9a2.25 2.25 0 00-2.25-2.25h-9A2.25 2.25 0 002.25 7.5v9a2.25 2.25 0 002.25 2.25z"
+                />
+              </svg>
+              <span>라이브</span>
+            </div>
+          </Link>
+          <Link href={'profile'}>
+            <div className="flex flex-col items-center space-y-2">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="h-6 w-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"
+                />
+              </svg>
+              <span>나의 캐럿</span>
+            </div>
+          </Link>
+        </nav>
+      )}
+    </div>
+  );
+}
+
+export default Layout;
